4b0d0283b9bb561f098da5eef84a42208a853fb1
Anne Bellefleur <annebellefleur@Annes-Air.attlocal.net>
2020-04-08 20:51:43 -0400

    pseudocode for game

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 console.log('Alien Hangman Game')
 
 
+//popup box appear w/intro to game
+
+//prompt user to input name
+
+//prompt user to pick game level
+
+//popup box will disappear
+
+//another popup will appear asking user to pick category
+
+//user name will be in top middle area
+
 
