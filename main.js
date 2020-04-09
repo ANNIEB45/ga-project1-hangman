46a5e5b935bcb34dc9c2f89593f074e6f095ff36
Anne Bellefleur <annebellefleur@Annes-Air.attlocal.net>
2020-04-08 20:52:36 -0400

    create variable of alphabet's for letterbox

--- main.js
+++ main.js
@@ -13,4 +13,9 @@ console.log('Alien Hangman Game')
 
 //user name will be in top middle area
 
+//letter box array
+var letterBoxArr =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+console.log(letterBoxArr)
 
+//choosing categories
