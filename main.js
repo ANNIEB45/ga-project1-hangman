7a4cb63f5bc9deeb6dee1090fdb1144fbebbca6f
Anne Bellefleur <annebellefleur@Annes-Air.attlocal.net>
2020-04-12 15:45:59 -0400

    starting all over again for better organization

--- main.js
+++ main.js
@@ -1,71 +1,24 @@
-// //variables for letters
+//variables for letters
 const alphabetForLetterBox = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+//Categories
+const categories = {
+    sports: ['baseball', 'cricket', 'judo', 'hang gliding', 'volleyball', 'yoga'],
 
-//global variables:
-const guessesLeft = document.querySelector('.guesses-left')
-const letterBox = document.querySelector('.letter-box')
-console.log(letterBox)
-
-
-
-//categories for the game- 
-const categories = ['baseball', 'hang gliding', 'audrey hepburn', 'rihanna',
-    'paris', 'kuala lumpa', 'los angelos', 'french horn', 'oboe', 'xylophone']
-
-// words will be randomized
-let chosenWord = categories[(Math.floor(Math.random()) * categories.length)]
-console.log(chosenWord)
-//where letters user select will appear
-const answerInput = document.querySelector('.answer-field')
-let answerInputField = []
-
-for (let i = 0; i < chosenWord.length; i++) {
-    answerInputField[i] = '_'
-    console.log(answerInputField)
+    'fashion icons': ['audrey hepburn', 'rihanna', 'mary and ashley olsen', 'princess diana', 'bob dylan', 'kanye west'],
+    'famous cities': ['paris', 'dubai', 'new york', 'kuala lumpa', 'istanbul', 'london', 'los angelos'],
+    'musical instruments': ['trombone', 'french horn', 'oboe', 'xylophone', 'synthesizer', 'lute', 'electric guitar']
 }
-answerInput.innerHTML = answerInputField.join(' ')
 
-//add hover and click event listener 
-letterBox.addEventListener('mouseover', (event) =>{
-    event.target.style.color = 'red'
-
-    setTimeout( () => {
-        event.target.style.color = ''
-    }, 800)
-})
-
-
-
-
-
-
-
-// //div with class letter-box
-//this function is to create the letter box to hold the alphabets
-const newLetter = () => {
-    //created a ul element
-    alphabetList = document.createElement('div')
-    for (let i = 0; i < alphabetForLetterBox.length; i++) {
-        alphabetList.class = 'alphabet-box'
-        //create list element
-        listEl = document.createElement('div')
-        listEl.class = 'squares'
-        //add the alphabet index to each li element
-        listEl.innerHTML = alphabetForLetterBox[i];
-        //append the ul to the div and the li to the ul
-        letterBox.appendChild(alphabetList)
-        alphabetList.appendChild(listEl)
-    }
-}
-console.log(newLetter())
-
-
-
-
-
-
-
+const startGameButton = ''
+const categories = ''
+const hint = ''
+const letterBox;
+const wrongLetters;
+const livesLeft = 6
+const userName;
+const score;
+const howToPlay;
 
 
